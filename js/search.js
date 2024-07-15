6cd6686605b5e44873e6f10186f0e608e3e60c40
PatelRohanS <rohanp4655@gmail.com>
2024-07-15 07:30:02 +0300

    add new wone

---
+++ js/search.js
@@ -0,0 +1,8 @@
+function openSearch() {
+    var input = document.getElementById("searchInput");
+    if (input.style.display === "block") {
+        input.style.display = "none";
+    } else {
+        input.style.display = "block";
+    }
+}
